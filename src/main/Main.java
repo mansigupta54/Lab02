1698ce3c84fb1daffea123c6aa23fa707168ea9d
mansigupta54 <mansi.gupta_cs18@gla.ac.in>
2019-08-22 15:10:41 +0530

    add file Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,15 +5,8 @@ package main;
  * @version 1.0
  */
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner val = new Scanner(System.in);
-        String n;
-        System.out.println("Enter the name");
-        n = val.next();
-        System.out.println("welcome, " + n);
 
     }
 }
